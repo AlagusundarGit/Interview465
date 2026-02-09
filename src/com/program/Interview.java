a3d6050a19a84bc8a71982547788dca170ec62cb
AlagusundarGit <asundar465@gmail.com>
2026-02-09 19:41:00 +0530

    Fixed error in Interview class

--- src/com/program/Interview.java
+++ src/com/program/Interview.java
@@ -2,17 +2,8 @@ package com.program;
 
 public class Interview {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		String s="techm";
-=======
 		String s="alagu";
 		String rev="";
->>>>>>> alagu
-=======
-		String s="alagu";
-		String rev="";
-		String s="techm";
->>>>>>> c67d398f7a10d068c945aadb3ccb4b6f7a3015ea
+		String ss="techm";
 	}
 }
