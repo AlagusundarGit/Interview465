b355877bc11b315ba47b7e84da6dbf6d68d8db58
AlagusundarGit <asundar465@gmail.com>
2026-02-09 20:01:22 +0530

    lines deleted

--- src/com/program/Interview.java
+++ src/com/program/Interview.java
@@ -3,10 +3,5 @@ package com.program;
 public class Interview {
 	public static void main(String[] args) {
 		String s="alagu";
-		String rev="";
-		for(int i=s.length()-1;i>=0;i--) {
-			rev=rev+s.charAt(i);
-		}
-		System.out.println(rev);
 	}
 }
